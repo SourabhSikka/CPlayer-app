5d5b5d54881cbad35cfdadea5b6ce06b5d6435cb
750586 <bodasingibhanuprasad@gmail.com>
2022-05-11 23:29:15 +0530

    Home-Page-added-navbar&footer

--- CplayerUI/src/app/app.module.ts
+++ CplayerUI/src/app/app.module.ts
@@ -3,19 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Player/home/home.component';
 import { DashboardComponent } from './Player/dashboard/dashboard.component';
+import { NavbarComponent } from './Player/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './player/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
