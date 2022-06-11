7956e6f519c2ea294105de316ea65cc1c05506e1
750586 <bodasingibhanuprasad@gmail.com>
2022-06-11 14:03:04 +0530

    Eureka Server added

--- Backend/userservice/src/main/java/com/stackroute/userservice/UserServiceApplication.java
+++ Backend/userservice/src/main/java/com/stackroute/userservice/UserServiceApplication.java
@@ -3,13 +3,14 @@ package com.stackroute.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-
+@EnableEurekaClient
 @SpringBootApplication
 @EnableScheduling
 @EnableSwagger2
